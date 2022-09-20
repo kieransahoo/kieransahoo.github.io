2c2454ad23e5ed65da7dd88f0f48e1ce368f8489
kieransahoo <kkieran50@gmail.com>
2022-09-20 05:51:52 +0530

    template changes

---
+++ main.js
@@ -0,0 +1,9 @@
+function toggleMenu(){
+    var menuToggle = document.querySelector(".toggle");
+    var menu = document.querySelector(".menu");
+    menuToggle.classList.toggle("active")
+    menu.classList.toggle("active")
+}
+
+// Scroll Animation
+
