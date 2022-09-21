d90726ba1f9bda4ad5267b3faa8cf6ddb31a841f
kieransahoo <kkieran50@gmail.com>
2022-09-21 17:26:00 +0530

    scroll-reveal

--- main.js
+++ main.js
@@ -6,3 +6,29 @@ function toggleMenu(){
 }
 
 
+const sr = ScrollReveal({
+    origin:'top',
+    distance:'85px',
+    duration:2000,
+    reset:true
+})
+
+sr.reveal(".home-text",{})
+sr.reveal(".home-img",{delay:200})
+
+sr.reveal(".about-img",{})
+sr.reveal(".about-text",{delay:200})
+sr.reveal(".social",{delay:200})
+
+sr.reveal(".content",{})
+sr.reveal(".tech-stack",{delay:200})
+
+sr.reveal(".project-content",{})
+sr.reveal(".box",{delay:200})
+
+sr.reveal(".calendar",{})
+
+sr.reveal(".contact-content",{})
+// sr.reveal(".contact-details",{delay:200})
+// sr.reveal(".contact-form",{delay:200})
+
