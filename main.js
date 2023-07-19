35e969deec7af9a15ef365d2cb6e70ced0d6d9a6
Kiran Kumar Sahoo <101393428+kieransahoo@users.noreply.github.com>
2023-07-19 12:42:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 let resume = document.getElementById("downloadresume");
 resume.addEventListener("click",function(){
-    window.open("https://drive.google.com/file/d/1_LAYkeKGjUSlkZEkgrSLDu_kaF2h7Hck/view?usp=sharing")
+    window.open("https://drive.google.com/file/d/1JyIGfXXuSUM-XOlQWQ-NYkgQqWubDfV9/view?usp=sharing")
 })
 
 let resume2 = document.getElementById("downloadresume2");
 resume2.addEventListener("click",function(){
-    window.open("https://drive.google.com/file/d/1_LAYkeKGjUSlkZEkgrSLDu_kaF2h7Hck/view?usp=sharing")
+    window.open("https://drive.google.com/file/d/1JyIGfXXuSUM-XOlQWQ-NYkgQqWubDfV9/view?usp=sharing")
 })
 
 function toggleMenu(){
